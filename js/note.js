93417475f2f1289f2277d0028a0a46ad4ffc0afd
Naim Hasan <naimarafat7@gmail.com>
2023-06-12 06:49:11 -0700

    note

---
+++ js/note.js
@@ -0,0 +1,27 @@
+/*
+JS Concepts:
+1. null vs undefined
+2. == vs ===
+3. scope: block, function, global
+4. clouser
+5.hoisting
+6.callback function 
+7. arguments. what is array like Object.
+8. array vs array like object
+9. truthy vs falsy
+10. ! vs !!
+
+*/
+
+/*
+
+Dev Tool
+------------------------------
+1.Elements Tab
+2.Console Tab
+3.Source
+4.Network
+5.Application
+6.Check Responsive
+
+*/
